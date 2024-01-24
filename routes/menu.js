20a827d9521df1664dd8a29852c4c03dc588a676
ryoponpon <ryopon.kr@gmail.com>
2024-01-24 19:39:47 +0900

    added menu banner

--- routes/menu.js
+++ routes/menu.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('menu', { title: '【公式】GENIE' });
+  res.render('menu');
 });
 
 module.exports = router;
